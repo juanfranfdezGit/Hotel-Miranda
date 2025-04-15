eb5f7e026c29330bcaeb79980fae9a0f733a0929
juanfranfdezGit <jfranromfdez@gmail.com>
2025-04-15 15:20:54 +0200

    feat: foods section

--- js/swiper.js
+++ js/swiper.js
@@ -1,20 +1,39 @@
 export default function swiper() {
-    const swiper = new Swiper('.swiper', {
-      effect: 'coverflow',
-      centeredSlides: true,
-      slidesPerView: 1, 
-      loop: true, 
-      coverflowEffect: {
-        rotate: 30,
-        stretch: 0, 
-        depth: 100,
-        modifier: 1, 
-        slideShadows: true, 
-      },
-      spaceBetween: 0, 
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  }
+  const mainSwiper = new Swiper('.swiper.main', {
+    effect: 'coverflow',
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 0,
+    coverflowEffect: {
+      rotate: 30,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next.main',
+      prevEl: '.swiper-button-prev.main',
+    },
+    pagination: {
+      el: '.swiper-pagination.main',
+    },
+  });
+
+  const menuSwiper = new Swiper('.swiper.menu', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next.menu',
+      prevEl: '.swiper-button-prev.menu',
+    },
+  });
+
+  const foodSwiper = new Swiper('.swiper.food', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination.food',
+      clickable: true,
+    },
+  });
+}
