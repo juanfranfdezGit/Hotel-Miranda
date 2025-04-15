928e145cf87ccbb07374e256cd115ff56c1f88ba
juanfranfdezGit <jfranromfdez@gmail.com>
2025-04-15 12:26:51 +0200

    feat: features function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import hamburguer from "./hamburguer.js"
 import availability from "./availability.js";
 import swiper from "./swiper.js";
+import features from "./features.js";
 
 addEventListener("DOMContentLoaded", () => {
     hamburguer();
     availability();
     swiper();
+    features();
 })
