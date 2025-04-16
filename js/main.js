b4fe6909c843a4c744211a8f1dd2d510fe217a6c
juanfranfdezGit <jfranromfdez@gmail.com>
2025-04-16 15:54:07 +0200

    feat: features dark test

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ import availability from "./availability.js";
 import swiper from "./swiper.js";
 import features from "./features.js";
 import header from "./header.js";
+import featuresDark from "./featuresDark.js";
 
 addEventListener("DOMContentLoaded", () => {
     hamburguer();
     availability();
     swiper();
     features();
+    featuresDark();
     header();
 })
