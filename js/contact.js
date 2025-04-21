8223c52dbb2a707e6351ad6d4b3c90040a2f2167
juanfranfdezGit <jfranromfdez@gmail.com>
2025-04-21 10:50:58 +0200

    feat: favico, some styles fixed

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,7 @@
 import hamburguer from "./hamburguer.js"
-import swiper from "./swiper.js";
 import header from "./header.js";
 
 addEventListener("DOMContentLoaded", () => {
     hamburguer();
-    swiper();
     header();
 })
